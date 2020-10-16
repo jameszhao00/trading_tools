2f7eb1d6d96156b572be0f5ef8ce8e4407b690c3
James Zhao <jameszhao00@gmail.com>
2020-10-16 00:01:32 -0400

    Update the portflio a bit.

--- portfolio_spec.py
+++ portfolio_spec.py
@@ -3,38 +3,46 @@ from portfolio_types import *
 # Major themes:
 # Stimulus delayed.
 
-portfolio = Portfolio(long_leverage=100, short_leverage=0, holdings=[
-    # Longs
+portfolio = Portfolio(long_leverage=100, short_leverage=60, holdings=[
+    # Sector growth.
+    Holding("PBW", 20, reason="Clean energy"),
 
-    # Growth
-    # What to look for:
-    # Small/medium Growth.
-    Holding("SQ", 8, reason="Leader in ECommerce payment solutions.", is_founder_active=True, glassdoor_ceo_rating=87),
-    Holding("DDOG", 10, reason="Leader in production monitoring", is_founder_active=True, glassdoor_ceo_rating=93),
+    # High Conviction.
+    Holding("DDOG", 10, reason="Leader in production monitoring", is_founder_active=True, glassdoor_ceo_rating=93,
+            hiring_surge=True),
     Holding("TWLO", 10, reason="Leader in communication IaaS and call centers", is_founder_active=True,
             glassdoor_ceo_rating=92, hiring_surge=True),
-    Holding("SE", 10, reason="Leader in Ecommerce in SE Asia", is_founder_active=True, glassdoor_ceo_rating=67),
-    Holding("NVDA", 10, reason="Leader in Machine learning", is_founder_active=True, glassdoor_ceo_rating=99),
-    Holding("FSLY", 10, reason="Leader in CDNs", is_founder_active=True, glassdoor_ceo_rating=100),
+    Holding("FSLY", 10, reason="Leader in CDNs", is_founder_active=True, glassdoor_ceo_rating=100, hiring_surge=True),
 
-    Holding("ZM", 4, reason="Leader in communication. Somewhat strong network effects.", is_founder_active=True,
-            glassdoor_ceo_rating=98),
-    Holding("PTON", 5, reason="Leader in home fitness", tactical="Buy more once it drops a bit.", is_founder_active=True,
-            glassdoor_ceo_rating=91),
-    Holding("ETSY", 4, reason="Leader in quirky online shopping. Different than amazon. Great moat.",
-            glassdoor_ceo_rating=82),
-    Holding("DOCU", 4, reason="Leader in document management.", tactical="Slowly build up a position?",
-            is_founder_active=True, glassdoor_ceo_rating=98),
-    Holding("TTD", 5, reason="???", tactical="Slowly build up a position?", is_founder_active=True, glassdoor_ceo_rating=87),
+    Holding("PTON", 10, reason="Leader in home fitness", is_founder_active=True, glassdoor_ceo_rating=91,
+            hiring_surge=True),
+    Holding("FIVN", 10, reason="Leader in contact center tech.", hiring_surge=True),
+    Holding("CRWD", 10, reason="Leader in endpoint security, needed for WFH", hiring_surge=True),
+    Holding("U", 10, reason="Leader in AR/VR", hiring_surge=True),
+
+    # Medium conviction.
+    Holding("NVDA", 5, reason="Leader in Machine learning", is_founder_active=True, glassdoor_ceo_rating=99),
+    Holding("ZM", 5, reason="Leader in communication. Somewhat strong network effects.", is_founder_active=True,
+            glassdoor_ceo_rating=98, hiring_surge=True),
+    Holding("SE", 5, reason="Leader in Ecommerce in SE Asia", is_founder_active=True, glassdoor_ceo_rating=67),
+    Holding("TTD", 5, reason="???", tactical="Slowly build up a position?", is_founder_active=True,
+            glassdoor_ceo_rating=87),
     Holding("ROKU", 5, reason="Leader in home entertainment", is_founder_active=True, glassdoor_ceo_rating=78,
             hiring_surge=True),
-    Holding("ZS", 3, reason="Security. Massive hiring surge."),
-    Holding("FIVN", 5, reason="Leader in contact center tech."),
+    Holding("NET", 5, reason="Launching WFH solution.", hiring_surge=True),
+    Holding("ZS", 5, reason="Security. Massive hiring surge.", hiring_surge=True),
+    Holding("OKTA", 5, reason="Leader in IAM, needed for WFH"),
+    Holding("DOCU", 5, reason="Leader in document management.", tactical="Slowly build up a position?",
+            is_founder_active=True, glassdoor_ceo_rating=98),
+    Holding("SQ", 5, reason="Leader in ECommerce payment solutions.", is_founder_active=True, glassdoor_ceo_rating=87),
+    Holding("NIO", 5, reason="Leader in chinese EVs.", is_founder_active=True, glassdoor_ceo_rating=87),
+    Holding("FRPT", 5, reason="Fresh food for pets."),
+    Holding("TRUP", 5, reason="Insurance administrator for pets."),
+    Holding("CHWY", 5, reason="More people are getting pets and seem to like chewy."),
 
-    Holding("PBW", 10, reason="Clean energy"),
     #
     # Holding("QQQ", -3, reason="Hedge."),
-    # Holding("WCLD", -1, reason="Hedge."),
+    Holding("WCLD", -1, reason="Hedge."),
     # Zillow CEO seems really MBA.
     # Holding("ZG", 5, reason="Leader in Home purchasing", is_founder_active=True, glassdoor_ceo_rating=93),
     # Holding("RDFN", 1, reason="Leader in Home purchasing", is_founder_active=True, glassdoor_ceo_rating=74),
